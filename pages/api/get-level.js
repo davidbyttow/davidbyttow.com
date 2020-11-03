2b6203cf33376e6349b260fdc58ed9173091eccc
David Byttow <david.byttow@gmail.com>
2020-11-02 19:23:56 -0500

    Adds title and deps for get-level API

--- pages/api/get-level.js
+++ pages/api/get-level.js
@@ -3,6 +3,7 @@ export default function handler(req, res) {
   res.setHeader('Content-Type', 'application/json')
   res.end(
     JSON.stringify({
+      title: 'Twisted Kingdom Test',
       scene: 'impossiblegames/assetbundles/dlc01_scene',
       deps: ['impossiblegames/assetbundles/dlc01_assets'],
     }),
